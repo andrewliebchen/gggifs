3f8dff6806943518cfe8aa9f4bece0c83b791ab0
Andrew Liebchen <andrewliebchen@gmail.com>
2016-04-24 17:52:44 -0700

    Return random gif

--- server/api.js
+++ server/api.js
@@ -13,12 +13,23 @@ Api.addCollection(Meteor.users, {
     get: {
       authRequired: false,
       action: function() {
-        const gif = Gifs.findOne({
-          parent_id: this.urlParams.id,
-          keywords: this.queryParams.keyword
-        });
+        const id = this.urlParams.id;
 
-        return { json: gif }
+        const keyword = this.queryParams.keyword;
+        const gifs = Gifs.find({
+          parent_id: id,
+          keywords: keyword
+        }).fetch();
+        const randomIndex = Math.floor( Math.random() * gifs.length );
+        const randomGif = gifs[randomIndex];
+        
+        const user = Meteor.users.findOne(id, {fields: {_id: 1, profile: 1}});
+
+        if(keyword) {
+          return { json: randomGif }
+        } else {
+          return { json: user };
+        }
       }
     }
   }
