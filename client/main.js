a01514ace061a4066865c8f8bb27f81fae7fe86e
Andrew Liebchen <andrewliebchen@gmail.com>
2016-05-06 15:19:39 -0700

    Don't do gif ranking yet

--- client/main.js
+++ client/main.js
@@ -41,12 +41,6 @@ Template.tags.helpers({
   }
 });
 
-Template.gifRanking.helpers({
-  ranking() {
-    return this.rank ? this.rank : 0
-  }
-})
-
 Template.search.events({
   'keypress .mtr-gif-search'(event, instance) {
     const keyword = event.target.value;
@@ -112,18 +106,6 @@ Template.singleTag.events({
       tag: this.data
     }, (err, res) => {
       console.log('Tag removed');
-    });
-  }
-});
-
-Template.gifRanking.events({
-  'click .mtr-rank-toggle'(event, instance) {
-    const clickedIndex = $(event.target).index();
-    Meteor.call('updateRank', {
-      id: this.id,
-      rank: clickedIndex
-    }, (err, res) => {
-      console.log('Rank updated');
     });
   }
 });
